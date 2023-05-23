60e80c9fc13864a01b36c95ed313c27749d8a29b
dsabag <dsabag@cisco.com>
2023-05-23 16:38:05 +0300

    wrap the json payload (after converted to NormalizedNode) with a ContainerNode and only then bind it to a java type ServiceEndpoints

--- service/src/main/java/com/example/Main.java
+++ service/src/main/java/com/example/Main.java
@@ -1,26 +1,26 @@
 package com.example;
 
 import com.google.gson.stream.JsonReader;
-import org.opendaylight.mdsal.binding.dom.adapter.ConstantAdapterContext;
-import org.opendaylight.mdsal.binding.dom.codec.api.BindingNormalizedNodeSerializer;
 import org.opendaylight.mdsal.binding.dom.codec.impl.BindingCodecContext;
 import org.opendaylight.mdsal.binding.generator.impl.DefaultBindingRuntimeGenerator;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeGenerator;
 import org.opendaylight.mdsal.binding.runtime.api.BindingRuntimeTypes;
 import org.opendaylight.mdsal.binding.runtime.api.DefaultBindingRuntimeContext;
 import org.opendaylight.mdsal.binding.runtime.spi.ModuleInfoSnapshotBuilder;
+import org.opendaylight.yang.gen.v1.urn.example2.norev.ServiceEndpoints;
+import org.opendaylight.yang.gen.v1.urn.example2.norev.service.endpoints.ServiceEndpoint;
 import org.opendaylight.yangtools.yang.binding.DataObject;
 import org.opendaylight.yangtools.yang.binding.InstanceIdentifier;
 import org.opendaylight.yangtools.yang.common.QName;
 import org.opendaylight.yangtools.yang.data.api.YangInstanceIdentifier;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNode;
-import org.opendaylight.yangtools.yang.data.api.schema.NormalizedNodes;
-import org.opendaylight.yangtools.yang.data.api.schema.SystemMapNode;
-import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactory;
+import org.opendaylight.yangtools.yang.data.api.schema.ContainerNode;
+import org.opendaylight.yangtools.yang.data.api.schema.MapEntryNode;
 import org.opendaylight.yangtools.yang.data.codec.gson.JSONCodecFactorySupplier;
 import org.opendaylight.yangtools.yang.data.codec.gson.JsonParserStream;
 import org.opendaylight.yangtools.yang.data.impl.schema.ImmutableNormalizedNodeStreamWriter;
 import org.opendaylight.yangtools.yang.data.impl.schema.NormalizedNodeResult;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableContainerNodeBuilder;
+import org.opendaylight.yangtools.yang.data.impl.schema.builder.impl.ImmutableMapNodeBuilder;
 import org.opendaylight.yangtools.yang.model.api.EffectiveModelContext;
 import org.opendaylight.yangtools.yang.model.repo.api.YangTextSchemaSource;
 import org.opendaylight.yangtools.yang.model.util.SchemaInferenceStack;
@@ -29,8 +29,8 @@ import org.opendaylight.yangtools.yang.parser.impl.DefaultYangParserFactory;
 
 import java.io.File;
 import java.io.StringReader;
+import java.util.Collection;
 import java.util.Map;
-import java.util.Optional;
 
 public class Main {
 
@@ -46,6 +46,7 @@ public class Main {
 
 	}
 
+
 	private static void try4() {
 
 		String yangDirectory = "/Users/dsabag/dev/yang-example/yang-model/src/main/yang/";
@@ -59,7 +60,6 @@ public class Main {
 				parser.addSource(source);
 			}
 			EffectiveModelContext context = parser.buildEffectiveModel();
-			JSONCodecFactory lhotkaCodecFactory = JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02.getShared(context);
 
 			final QName CONT = QName.create("urn:example2", "service-endpoints");
 
@@ -67,11 +67,11 @@ public class Main {
 			final var streamWriter = ImmutableNormalizedNodeStreamWriter.from(result);
 			final var jsonParser = JsonParserStream.create(
 							streamWriter,
-//							JSONCodecFactorySupplier.RFC7951.getShared(context),
-							lhotkaCodecFactory,
+							JSONCodecFactorySupplier.RFC7951.getShared(context),
+//							JSONCodecFactorySupplier.DRAFT_LHOTKA_NETMOD_YANG_JSON_02.getShared(context),
 							SchemaInferenceStack.Inference.ofDataTreePath(context, CONT)
-
 			);
+
 			jsonParser.parse(new JsonReader(new StringReader("""
 			{
 			  "Accedian-service-endpoint:service-endpoint": [
@@ -93,54 +93,40 @@ public class Main {
 			final var node = result.getResult();
 			System.out.println("node = " + node);
 
-			//
-			// example of extract data from node (dom based)
-			//
-			if(node instanceof SystemMapNode mapNode){
-				mapNode.body().forEach(inode -> {
-					Optional<NormalizedNode> optional = NormalizedNodes.getDirectChild(inode,
-									YangInstanceIdentifier.NodeIdentifier.create(QName.create(CONT, "endpoint-name")));
-					optional.ifPresent(normalizedNode -> System.out.println(normalizedNode.body()));
-				});
-			}
 
 			//
 			// convert the node to generated object
 			//
 
-			// one way to create ModuleInfoSnapshotBuilder
-//			final YangXPathParserFactory xpathFactory = new AntlrXPathParserFactory();
-//			DefaultYangParserFactory yangParserFactory = new DefaultYangParserFactory(xpathFactory);
-//			final ModuleInfoSnapshotBuilder moduleInfoSnapshotBuilder = new ModuleInfoSnapshotBuilder(yangParserFactory);
-//			moduleInfoSnapshotBuilder.add(org.opendaylight.yang.gen.v1.urn.example2.norev.$YangModuleInfoImpl.getInstance());
-
-			// another simpler way
 			final ModuleInfoSnapshotBuilder moduleInfoSnapshotBuilder = new ModuleInfoSnapshotBuilder(new DefaultYangParserFactory());
-
+			moduleInfoSnapshotBuilder.add(org.opendaylight.yang.gen.v1.urn.example2.norev.$YangModuleInfoImpl.getInstance());
 
 			final BindingRuntimeGenerator bindingRuntimeGenerator = new DefaultBindingRuntimeGenerator();
 			final BindingRuntimeTypes bindingRuntimeTypes = bindingRuntimeGenerator.generateTypeMapping(context);
 			DefaultBindingRuntimeContext bindingRuntimeContext = new DefaultBindingRuntimeContext(bindingRuntimeTypes, moduleInfoSnapshotBuilder.build());
 			BindingCodecContext bindingCodecContext = new BindingCodecContext(bindingRuntimeContext);
-			ConstantAdapterContext codec = new ConstantAdapterContext(bindingCodecContext);
-			BindingNormalizedNodeSerializer serializer = codec.currentSerializer();
+//			ConstantAdapterContext codec = new ConstantAdapterContext(bindingCodecContext);
+//			BindingNormalizedNodeSerializer serializer = codec.currentSerializer();
 
-			YangInstanceIdentifier yii = YangInstanceIdentifier.builder()
-							.node(QName.create(CONT, "service-endpoint"))
+
+
+			final YangInstanceIdentifier contYII = YangInstanceIdentifier.builder().node(ServiceEndpoints.QNAME).build();
+
+			ContainerNode containerNode = ImmutableContainerNodeBuilder.create()
+							.withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(ServiceEndpoints.QNAME))
+							.withChild(ImmutableMapNodeBuilder.create()
+											.withNodeIdentifier(YangInstanceIdentifier.NodeIdentifier.create(ServiceEndpoint.QNAME))
+											.withValue((Collection<MapEntryNode>) node.body())
+											.build()
+							)
 							.build();
 
-			Map.Entry<InstanceIdentifier<?>, DataObject> entry = serializer.fromNormalizedNode(YangInstanceIdentifier.of(CONT), node);
-			System.out.println("entry = " + entry);
+			System.out.println(containerNode.prettyTree());
 
-/* trying to iterate the node (which is a map)
-			if(node instanceof SystemMapNode mapNode) {
-				mapNode.body().forEach(inode -> {
-					Map.Entry<InstanceIdentifier<?>, DataObject> map =
-									codec.fromNormalizedNode(yii, inode);
-					System.out.println("map = " + map);
-				});
-			}
-*/
+			final Map.Entry<InstanceIdentifier<?>, DataObject> fromNormalizedNode = bindingCodecContext.fromNormalizedNode(contYII, containerNode);
+			final ServiceEndpoints value = (ServiceEndpoints) fromNormalizedNode.getValue();
+			System.out.println("endpoints = " + value);
+
 
 		} catch (Exception e) {
 			e.printStackTrace();
